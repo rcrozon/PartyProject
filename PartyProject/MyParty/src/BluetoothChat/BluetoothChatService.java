2dedf002f5e2bb593e48b56fb2b62006280e999f
rcrozon <romain.crozon@etu.u-bordeaux1.fr>
2014-03-20 13:01:18 +0100

    bluetooth

--- PartyProject/MyParty/src/BluetoothChat/BluetoothChatService.java
+++ PartyProject/MyParty/src/BluetoothChat/BluetoothChatService.java
@@ -383,19 +383,26 @@ public class BluetoothChatService {
 
             mmInStream = tmpIn;
             mmOutStream = tmpOut;
+            if (socket.isConnected())
+            	Log.i("Bluetooth", "BConnected");
+            else
+            	Log.i("Bluetooth", "BConnected");
         }
 
         public void run() {
             Log.i(TAG, "BEGIN mConnectedThread");
             byte[] buffer = new byte[1024];
             int bytes;
-
+            String s = "TEST";
+            write(s.getBytes());
             // Keep listening to the InputStream while connected
             while (true) {
                 try {
                     // Read from the InputStream
-                    bytes = mmInStream.read(buffer);
-
+                	Log.i(TAG, "BEGIN READ");
+                	bytes = mmInStream.read(buffer);
+                    Log.i(TAG, "END READ");
+                    
                     // Send the obtained bytes to the UI Activity
                     mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, bytes, -1, buffer)
                             .sendToTarget();
