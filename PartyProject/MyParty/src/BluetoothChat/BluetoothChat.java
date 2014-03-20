c35024c849ab618845a99a57111564cfc98dbdf5
rcrozon <romain.crozon@etu.u-bordeaux1.fr>
2014-03-20 12:32:52 +0100

    bluetooth

--- PartyProject/MyParty/src/BluetoothChat/BluetoothChat.java
+++ PartyProject/MyParty/src/BluetoothChat/BluetoothChat.java
@@ -180,13 +180,12 @@ public class BluetoothChat {
                 byte[] writeBuf = (byte[]) msg.obj;
                 // construct a string from the buffer
                 String writeMessage = new String(writeBuf);
-                mConversationArrayAdapter.add("Me:  " + writeMessage);
                 break;
             case MESSAGE_READ:
                 byte[] readBuf = (byte[]) msg.obj;
                 // construct a string from the valid bytes in the buffer
                 String readMessage = new String(readBuf, 0, msg.arg1);
-                mConversationArrayAdapter.add(mConnectedDeviceName+":  " + readMessage);
+                Log.i("MESSAGE RECEIVED", readMessage);
                 break;
             case MESSAGE_DEVICE_NAME:
                 // save the connected device's name
