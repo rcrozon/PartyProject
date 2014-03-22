01f4b594e09d19676d7f637690782a9cf1987943
rcrozon <romain.crozon@etu.u-bordeaux1.fr>
2014-03-22 12:34:39 +0100

    amelioration android cleint et connexion client application windows phone

---
+++ PartyProject/MyPartyProjectWP/MyPartyProject/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPartyProject.Entities
+{
+    public class Client
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string mail { get; set; }
+        public string password { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string admin { get; set; }
+        public string created { get; set; }
+    }
+}
